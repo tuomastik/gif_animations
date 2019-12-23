ae0012c72bbc4e43bf9b2c540b6df5046ebd050a
Tuomas Tikkanen <tuomas.tikkanen@hotmail.com>
2019-12-23 11:01:43 +0200

    Add command-line interface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from pprint import pprint
 from sys import platform as _platform
 
 import projections
@@ -36,33 +37,39 @@ def convert_images_format(ims_folder, format_from="svg", format_to="png",
 
 def create_gif(ims_input_folder, gif_output_name, delay=2, ext="png"):
     # Create GIF with ImageMagick
-    print("Stitching together a GIF from the frames of directory: %s..." % ims_input_folder)
+    print(f"Stitching together a GIF from the frames "
+          "in the directory: {ims_input_folder}...")
     subprocess.call(
         "{path_to_convert} -delay {delay} "
         "{ims_folder}/*{ext} {gif_name}".format(
             path_to_convert=get_imagemagick_path(), delay=delay, ext=ext,
             ims_folder=ims_input_folder, gif_name=gif_output_name), shell=True)
+    print(f"The following GIF was created successfully: {gif_output_name}")
 
 
 if __name__ == '__main__':
-    frames_folder = projections.create_animation_frames()
-    create_gif(ims_input_folder=frames_folder,
-               gif_output_name='projections.gif', delay=3)
+    gifs = {module.__name__: module for module
+            in [projections, cubes, mountain, kde, christmas]}
 
-    frames_folder = cubes.create_animation_frames()
-    create_gif(ims_input_folder=frames_folder,
-               gif_output_name='cubes.gif', delay=3)
+    print("Which GIF do you want to create?")
+    pprint({i: g for i, g in enumerate(gifs.keys())})
+    gif_id = int(input("Give the number of the GIF: "))
+    gif_module = gifs[list(gifs.keys())[gif_id]]
+    gif_name = gif_module.__name__
 
-    frames_folder = mountain.create_animation_frames()
-    create_gif(ims_input_folder=frames_folder,
-               gif_output_name='mountain.gif', delay=4)
+    frames_folder = gif_module.create_animation_frames()
 
-    frames_folder = kde.create_animation_frames()
-    convert_images_format(frames_folder, format_from="png", format_to="png",
-                          resolution="500x500", create_new_ims=False)
-    create_gif(ims_input_folder=frames_folder,
-               gif_output_name='kde.gif', delay=4)
+    if gif_name == kde.__name__:
+        convert_images_format(frames_folder, format_from="png", format_to="png",
+                              resolution="500x500", create_new_ims=False)
 
-    frames_folder = christmas.create_animation_frames()
+    gif_creation_options = {
+        projections.__name__: {"delay": 3},
+        cubes.__name__: {"delay": 3},
+        mountain.__name__: {"delay": 4},
+        kde.__name__: {"delay": 4},
+        christmas.__name__: {"delay": 2},
+    }
     create_gif(ims_input_folder=frames_folder,
-               gif_output_name='christmas.gif', delay=2)
+               gif_output_name=gif_name + ".gif",
+               **gif_creation_options[gif_name])
